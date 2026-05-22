2526207d8289a9da2c3a75817c521c3c65a72355
Rohit Ranjan Borah <rohit180b@gmail.com>
2026-05-22 22:08:10 +0530

    2nd commit

--- react-app/vite.config.js
+++ react-app/vite.config.js
@@ -1,6 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()]
-})
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      host: process.env.VITE_HOST || 'localhost'
+    }
+  })
+}
